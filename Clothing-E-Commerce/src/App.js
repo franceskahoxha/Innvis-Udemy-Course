6f0d84c3c131f54ddd4054b5babf8b11e6394fff
fhoxha-innvis <franceska.hoxha@innvis.io>
2023-05-23 19:31:06 +0200

    component folder added

--- Clothing-E-Commerce/src/App.js
+++ Clothing-E-Commerce/src/App.js
@@ -1,55 +1,9 @@
-import "./category-item.scss";
+import CategoryContainer from "./components/CategoryContainer/CategoryContainer";
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      subTitle: "shop now",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      subTitle: "shop now",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      subTitle: "shop now",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      subTitle: "shop now",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      subTitle: "shop now",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
-    <div className="App">
-      <div className="categories-container">
-        {categories.map(({ title, subTitle, id, imageUrl }) => (
-          <div key={id} className="category-container">
-            <div
-              className="background-image"
-              style={{ backgroundImage: `url(${imageUrl})` }}
-            />
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>{subTitle}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <>
+      <CategoryContainer />
+    </>
   );
 }
 
