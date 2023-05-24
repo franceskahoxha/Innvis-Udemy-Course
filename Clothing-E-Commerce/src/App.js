88043174d67d4ca44a8be754a3d322f4c82cb0ae
fhoxha-innvis <franceska.hoxha@innvis.io>
2023-05-24 15:28:52 +0200

    creating routes

--- Clothing-E-Commerce/src/App.js
+++ Clothing-E-Commerce/src/App.js
@@ -1,9 +1,21 @@
-import CategoryContainer from "./components/CategoryContainer/CategoryContainer";
+import Homepage from "./routes/Homepage/Homepage";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/Navigation/Navigation";
 function App() {
+  const Shop = () => {
+    return (
+      <div>
+        <div>I am a shop</div>
+      </div>
+    );
+  };
   return (
-    <>
-      <CategoryContainer />
-    </>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Homepage />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 }
 
